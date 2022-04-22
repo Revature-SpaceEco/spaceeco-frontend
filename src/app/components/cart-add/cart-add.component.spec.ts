e048a3684413d98d32995e8bccd7b308694ff492
Jiwon Shim <jshim0209@hotmail.com>
2022-04-22 16:50:46 -0400

    test fail fix

--- src/app/components/cart-add/cart-add.component.spec.ts
+++ src/app/components/cart-add/cart-add.component.spec.ts
@@ -1,5 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { CartAddComponent } from './cart-add.component';
 
 describe('CartAddComponent', () => {
@@ -8,6 +8,7 @@ describe('CartAddComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ HttpClientTestingModule],
       declarations: [ CartAddComponent ]
     })
     .compileComponents();
