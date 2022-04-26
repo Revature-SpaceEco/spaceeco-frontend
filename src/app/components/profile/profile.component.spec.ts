4aa3a35fed89b0d74e1a4226c7dee5dd13cdfea2
Garrett McPherson <captain_blue_of_eg@yahoo.com>
2022-04-26 07:53:53 -0700

    add dependencies to profile test

--- src/app/components/profile/profile.component.spec.ts
+++ src/app/components/profile/profile.component.spec.ts
@@ -1,20 +1,27 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { AddressServiceService } from 'src/app/services/address-service.service';
+import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import { ProfileComponent } from './profile.component';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
+  let mockAddressSerivce: any;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ProfileComponent ]
+      imports: [HttpClientTestingModule],
+      declarations: [ ProfileComponent ],
+      providers: [AddressServiceService]
+        //{provide: AddressServiceService, useValue: mockAddressSerivce}
+      //]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfileComponent);
+    mockAddressSerivce = TestBed.inject(AddressServiceService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
