f0016cf6a81cfd293a1cff87bc0b333b041bd32c
Garrett McPherson <captain_blue_of_eg@yahoo.com>
2022-04-26 08:08:00 -0700

    fixed app routing module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,15 @@ import { CartAddComponent } from './components/cart-add/cart-add.component';
 import { DisplayProductsComponent } from './components/display-products/display-products.component';
 import { AuthComponent } from './components/auth/auth.component';
 import { ProfileComponent } from './components/profile/profile.component';
+import { RegisterComponent } from './components/register/register.component';
+
 
 const routes: Routes = [
   {path: 'cart-add', component: CartAddComponent},
   {path: '', component: DisplayProductsComponent},
   {path: 'products/:id', component: CartAddComponent},
   {path:'login', component: AuthComponent},
+  { path: 'register', component: RegisterComponent },
   {path:'profile', component: ProfileComponent}
 ]
 
