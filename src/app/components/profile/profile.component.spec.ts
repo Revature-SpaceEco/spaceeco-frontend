2ecf6d11b3b065650f008fe6c5bd7fb03cb79ded
Serena Guo <syg9345@gmail.com>
2022-04-28 10:38:41 -0400

    adding unit test for component and service

--- src/app/components/profile/profile.component.spec.ts
+++ src/app/components/profile/profile.component.spec.ts
@@ -1,27 +1,41 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AddressServiceService } from 'src/app/services/address-service.service';
-import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
 import { ProfileComponent } from './profile.component';
+import { of } from 'rxjs';
+import { Address } from 'src/app/models/Address';
 
 describe('ProfileComponent', () => {
   let component: ProfileComponent;
   let fixture: ComponentFixture<ProfileComponent>;
-  let mockAddressSerivce: any;
-
+  let getAddressSpy: { calls: { any: () => any; }; };
+  let planetEl: any;
+  let address: Address;
   beforeEach(async () => {
+
+    address= {
+      id: 1,
+      addressLineOne: "9194 North College Ave",
+      addressLineTwo: "512 South Pennsylvania St",
+      city: "Grand Haven",
+      state: "MI",
+      country: "United States",
+      zip: "49417",
+      solarSystem: "solar system",
+      planet: "earth"
+    };
+
+    let mockAddressSerivce = jasmine.createSpyObj('AddressServiceService', ['getAddress']);
+
+    getAddressSpy = mockAddressSerivce.getAddress.and.returnValue(of(address))
     await TestBed.configureTestingModule({
-      imports: [HttpClientTestingModule],
       declarations: [ ProfileComponent ],
-      providers: [AddressServiceService]
-        //{provide: AddressServiceService, useValue: mockAddressSerivce}
-      //]
+      providers: [{provide: AddressServiceService, useValue: mockAddressSerivce}]
     })
     .compileComponents();
   });
 
   beforeEach(() => {
     fixture = TestBed.createComponent(ProfileComponent);
-    mockAddressSerivce = TestBed.inject(AddressServiceService);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
@@ -29,4 +43,10 @@ describe('ProfileComponent', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should show address after component initialized', () =>{
+    planetEl = fixture.nativeElement.querySelector('.planet');
+    expect(planetEl.textContent).toBe(address.planet);
+    expect(getAddressSpy.calls.any()).withContext('getAddress called').toBe(true);
+  })
 });
--- src/app/services/address-service.service.spec.ts
+++ src/app/services/address-service.service.spec.ts
@@ -1,19 +1,18 @@
 import { TestBed } from '@angular/core/testing';
 
 import { AddressServiceService } from './address-service.service';
-import {HttpClientTestingModule, HttpTestingController} from "@angular/common/http/testing";
+import { HttpClientTestingModule, HttpTestingController } from "@angular/common/http/testing";
+import { Address } from '../models/Address';
 
 describe('AddressServiceService', () => {
   let URL = 'http://localhost:8080';
   let service: AddressServiceService;
   let httpTestingController: HttpTestingController;
   beforeEach(() => {
-    
+
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [
-        AddressServiceService
-      ]
+      providers: [AddressServiceService]
     });
     service = TestBed.inject(AddressServiceService);
     httpTestingController = TestBed.inject(HttpTestingController);
@@ -23,16 +22,16 @@ describe('AddressServiceService', () => {
     expect(service).toBeTruthy();
   });
 
-  describe("get address", () => {
-    /*
-    it('should call the correct url', () => {
-      service.getAddress().subscribe(address => {
-        expect(address.id).toBe(1);
-      });
 
-      const req = httpTestingController.expectOne(URL + '/address/users/1');
+  it('should GET correct address', () => {
+    let actualAddress: Address | undefined;
+    service.getAddress().subscribe(address => {
+      actualAddress = address
+    })
+    const req = httpTestingController.expectOne(URL + '/address/users/1');
 
-      req.flush({id: 1,
+    req.flush({
+      id: 1,
       addressLineOne: "9194 North College Ave",
       addressLineTwo: "512 South Pennsylvania St",
       city: "Grand Haven",
@@ -40,10 +39,13 @@ describe('AddressServiceService', () => {
       country: "United States",
       zip: "49417",
       solarSystem: "solar system",
-      planet: "earth"});
-      expect(req.request.method).toBe('GET');
-      httpTestingController.verify();
-    })*/
+      planet: "earth"
+    });
+
+    httpTestingController.verify();
+    expect(req.request.method).toBe('GET');
+    expect(actualAddress?.id).toBe(1);
+
   })
-  
+
 });
