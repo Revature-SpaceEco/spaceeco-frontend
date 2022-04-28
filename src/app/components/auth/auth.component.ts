4fab1963e745a44f4410c4289a2bb24447c32c83
George Balch <george089@revature.net>
2022-04-27 21:43:05 -0700

    Display Invalid username/password msg on login failure

--- src/app/components/auth/auth.component.ts
+++ src/app/components/auth/auth.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import { NgForm } from '@angular/forms';
 import { AuthService } from 'src/app/services/auth.service';
 import { Router } from '@angular/router';
@@ -10,7 +10,12 @@ import { Router } from '@angular/router';
 })
 export class AuthComponent implements OnInit {
 
-  constructor(private authService: AuthService, private router: Router) { }
+  @ViewChild('errorMsg') errorMsg: ElementRef;
+
+  constructor(
+    private authService: AuthService, 
+    private router: Router
+  ) {}
 
   ngOnInit(): void {
     
@@ -28,7 +33,9 @@ export class AuthComponent implements OnInit {
           this.router.navigate(['/profile']);
         },
         error: (e) => {
-          console.log(e)
+          if (e.status == 403) {
+            this.errorMsg.nativeElement.style.display = 'block';
+          }
         },
       });
     form.reset();
