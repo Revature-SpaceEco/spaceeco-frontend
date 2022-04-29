0ec9fc3c85692c14f8137a768ad29d1229fd3d17
Garrett McPherson <captain_blue_of_eg@yahoo.com>
2022-04-29 13:14:54 -0700

    restored karma config file

--- karma.conf.js
+++ karma.conf.js
@@ -37,9 +37,9 @@ module.exports = function (config) {
     port: 9876,
     colors: true,
     logLevel: config.LOG_INFO,
-    autoWatch: true,
-    browsers: ['Chrome'],
-    singleRun: false,
-    restartOnFileChange: true
+    autoWatch: false,
+    browsers: ['FirefoxHeadless'],
+    singleRun: true,
+    restartOnFileChange: false
   });
 };
