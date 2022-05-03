c2f09857297473697710074b71abf8d9d522bbde
Caitlin T <90787453+cait-tal@users.noreply.github.com>
2022-05-03 10:06:26 -0600

    Merge conflicts resolved.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,8 +6,13 @@ import { AuthComponent } from './components/auth/auth.component';
 import { ProfileComponent } from './components/profile/profile.component';
 import { RegisterComponent } from './components/register/register.component';
 import { CartCheckoutComponent } from './components/cart-checkout/cart-checkout.component';
+<<<<<<< Updated upstream
 import { OrderCompletedComponent } from './components/order-completed/order-completed.component';
 import { ErrorPageComponent } from './components/error-page/error-page.component';
+=======
+import { BillingComponent } from './components/billing/billing.component'
+import { OrderCompletedComponent } from './components/order-completed/order-completed.component';
+>>>>>>> Stashed changes
 
 const routes: Routes = [
   { path: '', component: DisplayProductsComponent },
@@ -16,8 +21,13 @@ const routes: Routes = [
   { path:'login', component: AuthComponent},
   { path: 'register', component: RegisterComponent },
   { path: 'checkout', component: CartCheckoutComponent },
+<<<<<<< Updated upstream
   { path: 'order', component: OrderCompletedComponent },
   { path: '**', component: ErrorPageComponent }, // Create a 404 Page
+=======
+  { path: 'order-success', component: OrderCompletedComponent},
+  { path: '**', component: DisplayProductsComponent }, // Create a 404 Page
+>>>>>>> Stashed changes
 ];
 
 
--- src/app/components/cart-checkout/cart-checkout.component.ts
+++ src/app/components/cart-checkout/cart-checkout.component.ts
@@ -30,7 +30,11 @@ export class CartCheckoutComponent implements OnInit {
     }
 
   ngOnInit(): void {
+<<<<<<< Updated upstream
     this.currentStep = 1;
+=======
+    this.currentStep = 1; 
+>>>>>>> Stashed changes
   }
 
   goToNextStep(step: number) {
