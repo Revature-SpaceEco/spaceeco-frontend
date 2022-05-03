509ed7903de49d39fcc3dcec2d421d43e98e0e3a
George Balch <george089@revature.net>
2022-05-03 15:37:12 -0700

    Git trickery

---
+++ src/app/services/user/user.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+const url = environment.BACKEND_URL;
+
+@Injectable({
+  providedIn: 'root',
+})
+export class UserService {
+  userId = localStorage.getItem('userId');
+  constructor(private httpClient: HttpClient) {}
+
+  httpOptions = {
+    headers: new HttpHeaders({
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Headers': 'X-Requested-With, Content-Type, Origin, Authorization, Accept, Client-Security-Token, Accept-Encoding, X-Auth-Token'
+    })
+  }; 
+
+  getUserById(): Observable<any> {
+    return this.httpClient.get<any>(url + '/users/' + this.userId,
+                                   this.httpOptions);
+  }
+}
