94701fb0bf7d3f4dad84a52ee9dc204b5c49f669
Caitlin T <90787453+cait-tal@users.noreply.github.com>
2022-05-04 10:34:31 -0600

    Adjust models on frontend.

--- src/app/models/Payment.ts
+++ src/app/models/Payment.ts
@@ -2,6 +2,6 @@ import { BillingDetails } from "./BillingDetails";
 
 export interface Payment {
     id: number;
-    paymentStatus: string;
-    paymentBillingDetails: BillingDetails;
+    billingDetails: BillingDetails;
+    status: string;
 }
